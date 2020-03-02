127e41caf0d372f20f3c6812093c7cf76fb5aee0
prakash1 <prakashjayaswal62@gmail.com>
2020-03-02 19:37:41 +0530

    bassic logger added in master branch

--- server.js
+++ server.js
@@ -2,14 +2,16 @@ var express = require('express')
 var auth = require('./middleware/authenticate')
 var app = express();
 
-app.get('/',function(Req,res){
+function basicLogger(req,res,next){
+    console.log(req.url)
+    next()
+}
+
+app.get('/',basicLogger,function(Req,res){
     res.send("hello world")
 })
-app.get('/users',auth,function(req,res){
-    res.send("user route")
-})
 
-app.get('/users',auth,function(req,res){
+app.get('/users',basicLogger,auth,function(req,res){
     res.send("users Route")
 })
 app.listen(1234,function(){
