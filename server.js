623fb972dbd8b508000dd2e1674e488e187b803c
prakash1 <prakashjayaswal62@gmail.com>
2020-03-02 19:28:02 +0530

    middleware added to feature user branch

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var express = require('express')
-
+var auth = require('./middleware/authenticate')
 var app = express();
 
 app.get('/',function(Req,res){
     res.send("hello world")
 })
 
-app.get('/users',function(req,res){
+app.get('/users',auth,function(req,res){
     res.send("users Route")
 })
 app.listen(1234,function(){
