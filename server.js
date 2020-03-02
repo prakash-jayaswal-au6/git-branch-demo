2d9c41aa37ba020929481d6ea6bad3f010daa0cd
prakash1 <prakashjayaswal62@gmail.com>
2020-03-02 16:52:53 +0530

    users route to feature-user

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ app.get('/',function(Req,res){
     res.send("hello world")
 })
 
+app.get('/users',function(req,res){
+    res.send("users Route")
+})
 app.listen(1234,function(){
     console.log("server started at port 1234")
 })
